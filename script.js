11f6f5e92ee5e15e0163c157535398f81ba52939
Joshua Hernandez <jedchernandez@gmail.com>
2021-05-27 22:37:24 -0400

    feat: Add loader

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+// Show Loading
+const loading = () => {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+};
+
+// Hide Loading
+const complete = () => {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+};
+
 // Show New Quote
 let newQuote = () => {
+  loading();
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   //   Check if author field is null/blank and replace it with 'Unknown'
@@ -22,14 +36,16 @@ let newQuote = () => {
   } else {
     quoteText.classList.remove("long-quote");
   }
+  //   Set Quote, Hide Loader
   quoteText.textContent = quote.text;
+  complete();
 };
 
 // Tweet Quote
-function tweetQuote() {
+const tweetQuote = () => {
   const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
   window.open(twitterUrl, "_blank");
-}
+};
 
 // Event Listeners
 newQuoteBtn.addEventListener("click", newQuote);
@@ -37,6 +53,7 @@ twitterBtn.addEventListener("click", tweetQuote);
 
 // Get Quotes From API
 async function getQuotes() {
+  loading();
   const apiURL = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiURL);
